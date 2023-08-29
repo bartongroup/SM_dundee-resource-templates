6cd7d694c9584b35f327fe45f622ceea22d11268
Stuart MacGowan <smacgowan@dundee.ac.uk>
2023-08-29 13:00:19 +0100

    Refactor SubmissionHandler
    - Introduced class attributes to share state across methods.
    - Renamed methods for better readability and understanding.
    - Added docstrings to methods.

--- submission.py
+++ submission.py
@@ -10,74 +10,92 @@ from session_db import insert_metadata, update_status
 custom_logger = setup_logging(name='submission')
 
 class SubmissionHandler:
+    """Handles FASTA file submissions and associated processing."""
 
     def __init__(self, session_id, form):
+        """Initialize a SubmissionHandler instance.
+
+        Args:
+            session_id (str): Unique identifier for the submission session.
+            form (FlaskForm): Form object containing the submission details.
+        """
         self.session_id = session_id
         self.form = form
         self.session_directory = self.create_directory()
-    
+        self.fasta_filename = None
+        self.file_path = None
+
     def create_directory(self):
-        # Create a directory for the submission
+        """Create a directory for the submission session.
+
+        Returns:
+            str: The path to the created directory.
+        """
         session_directory = os.path.join(SESSIONS_FOLDER, self.session_id)
         os.makedirs(session_directory, exist_ok=True)
         custom_logger.debug(f"Directory created for session {self.session_id}.")
         return session_directory
     
-    def save_fasta_file(self):
+    def save_submission_data(self):
+        """Save the uploaded FASTA file or the input sequence."""
         if self.form.fasta_file.data:
-            fasta_filename = self.form.fasta_file.data.filename
-            file_path = os.path.join(self.session_directory, fasta_filename)
-            self.form.fasta_file.data.save(file_path)
-            custom_logger.info(f"FASTA file uploaded for session {self.session_id}.")
+            self.fasta_filename = self.form.fasta_file.data.filename
+            self.file_path = os.path.join(self.session_directory, self.fasta_filename)
+            self.form.fasta_file.data.save(self.file_path)
         else:
-            fasta_filename = 'sequence.fasta'
-            file_path = os.path.join(self.session_directory, fasta_filename)
-            with open(file_path, 'w') as f:
+            self.fasta_filename = 'sequence.fasta'
+            self.file_path = os.path.join(self.session_directory, self.fasta_filename)
+            with open(self.file_path, 'w') as f:
                 f.write(self.form.sequence.data)
-            custom_logger.info(f"Sequence data saved for session {self.session_id}.")
-        return fasta_filename, file_path
+        custom_logger.info(f"FASTA data saved for session {self.session_id}.")
 
-    def insert_db_metadata(self, fasta_filename):
-        insert_metadata(self.session_id, fasta_filename, 'output.fasta', 'uploaded', 
-                            (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d %H:%M:%S'))
+    def store_submission_metadata(self):
+        """Insert metadata related to the submission into the database."""
+        expiration_time = (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d %H:%M:%S')
+        insert_metadata(self.session_id, self.fasta_filename, 'output.fasta', 'uploaded', expiration_time)
         custom_logger.info(f"Metadata inserted into database for session {self.session_id}.")
 
-    def read_fasta_file(self, file_path):
-        with open(file_path, 'r') as f:
-            fasta_content = f.read()
-        return fasta_content
+    def read_cached_submission(self):
+        """Read the saved FASTA file.
+
+        Returns:
+            str: The content of the FASTA file.
+        """
+        with open(self.file_path, 'r') as f:
+            return f.read()
 
     def process_and_save_results(self, fasta_content):
+        """Process the FASTA file content and save the results."""
         output_content = process_fasta(fasta_content)
         output_file_path = os.path.join(self.session_directory, 'output.fasta')
-            
         with open(output_file_path, 'w') as f:
             f.write(output_content)
 
-    def update_db_status(self, fasta_filename):
-        update_status(self.session_id, fasta_filename, "processed")
+    def update_db_status(self):
+        """Update the processing status in the database."""
+        update_status(self.session_id, self.fasta_filename, "processed")
         custom_logger.info(f"FASTA file processed and status updated for session {self.session_id}.")
 
-    def handle_submission(self):        
+    def handle_submission(self):
+        """Handle the submission by orchestrating the various steps.
+
+        Returns:
+            dict: A dictionary containing the status, message, and other details of the submission.
+        """
         result = {'status': 'failed', 'message': '', 'filename': None}
-        
+
         try:
-            fasta_filename, file_path = self.save_fasta_file()
-
-            self.insert_db_metadata(fasta_filename)
-
-            fasta_content = self.read_fasta_file(file_path)
-
+            self.save_submission_data()
+            self.store_submission_metadata()
+            fasta_content = self.read_cached_submission()
             self.process_and_save_results(fasta_content)
-            
-            self.update_db_status(fasta_filename)
-
-            # Return the result
-            result['status'] = 'success'
-            result['message'] = 'File processed successfully.'
-            result['directory'] = self.session_directory
-            result['filename'] = fasta_filename
-            
+            self.update_db_status()
+            result.update({
+                'status': 'success',
+                'message': 'File processed successfully.',
+                'directory': self.session_directory,
+                'filename': self.fasta_filename
+            })
         except Exception as e:
             custom_logger.error(f"An error occurred while handling submission for session {self.session_id}: {str(e)}")
             result['status'] = 'failed'
