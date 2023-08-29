3dd0049d86c4a9f9c4ba71c26ea9de66d095fd80
Stuart MacGowan <smacgowan@dundee.ac.uk>
2023-08-29 13:20:56 +0100

    Refactor logging and capture werkzeug to file
    - Add special handling to configure werkzeug logger when the logger name
      is 'app'.

--- logger_config.py
+++ logger_config.py
@@ -1,3 +1,5 @@
+# logger_config.py
+
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -14,13 +16,22 @@ def setup_logging(name='custom'):
     # Set up log formatting
     formatter = logging.Formatter('[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(name)s - %(message)s')
 
-    # Set up log rotation
-    log_handler = RotatingFileHandler(log_file, maxBytes=1_000_000, backupCount=5)
-    log_handler.setFormatter(formatter)
+    # Set up log rotation for custom logger
+    custom_handler = RotatingFileHandler(log_file, maxBytes=1_000_000, backupCount=5)
+    custom_handler.setFormatter(formatter)
 
-    # Your custom logger
+    # Custom logger
     custom_logger = logging.getLogger(name)
     custom_logger.setLevel(logging.INFO)
-    custom_logger.addHandler(log_handler)
+    custom_logger.addHandler(custom_handler)
     
+    # Set up log rotation for werkzeug logger if the name is 'app'
+    if name == 'app':
+        werkzeug_handler = RotatingFileHandler(os.path.join(LOG_PATH, 'werkzeug.log'), maxBytes=1_000_000, backupCount=5)
+        werkzeug_handler.setFormatter(formatter)
+
+        werkzeug_logger = logging.getLogger('werkzeug')
+        werkzeug_logger.setLevel(logging.INFO)
+        werkzeug_logger.addHandler(werkzeug_handler)
+
     return custom_logger
