a7f1321ae03e52b7b97bc9b77acb12c750954d14
Stuart MacGowan <smacgowan@dundee.ac.uk>
2024-11-12 20:26:29 +0000

    Add LigysisForm with validation for UniProt ID and multiple file uploads

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, FileField, SubmitField
+from wtforms import TextAreaField, StringField, FileField, MultipleFileField, SubmitField
 
 
 class FastaForm(FlaskForm):
@@ -19,6 +19,28 @@ class FastaForm(FlaskForm):
         # If neither field has data, add a form-wide error
         if not self.sequence.data and not self.fasta_file.data:
             self.sequence.errors.append('Either enter a FASTA sequence or upload a FASTA file.')
+            return False
+
+        return False
+
+
+class LigysisForm(FlaskForm):
+    uniprot_id = StringField('Enter UniProt ID')
+    files = MultipleFileField('and upload mmCiF files')
+    submit = SubmitField('Submit')
+
+    # Custom validator
+    def validate(self, extra_validators=None):
+        # Use the default validate method first
+        initial_validation = super(LigysisForm, self).validate(extra_validators=extra_validators)
+
+        # If the initial validation passes and both parts have data, return True
+        if initial_validation and (self.uniprot_id.data and self.files.data):
+            return True
+
+        # If either field is missing, add a form-wide error
+        if not (self.uniprot_id.data and self.files.data):
+            self.uniprot_id.errors.append('Please enter UniProt ID and upload mmCiF files.')
             return False
 
         return False
