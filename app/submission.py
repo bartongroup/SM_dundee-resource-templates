7efbb967c770b9966b44148fbe2061aa5e5b4bed
Stuart MacGowan <smacgowan@dundee.ac.uk>
2024-11-28 13:09:13 +0000

    Add TODO to consider returning job ID from process_file in SubmissionHandler

--- app/submission.py
+++ app/submission.py
@@ -116,6 +116,7 @@ class SubmissionHandler:
         """Process the FASTA file content and save the results."""
         processor = SlivkaProcessor(SLIVKA_URL, service=self.service_type, session_id=self.session_id, filename=self.filename, entry_id=self.entry_id, config=self.config)
         output_file_path = os.path.join(self.submission_directory, 'output.fasta')
+        # TODO: Consider returning job ID from process_file and updating the database with the job ID here outside the SlivkaProcessor class
         success = processor.process_file(self.file_path, output_file_path, self.submission_directory)
 
     def update_db_status(self):
